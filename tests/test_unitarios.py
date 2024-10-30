33170d0b682aa776bd81b29c80b78a16909b224a
Pedro <pedro.sa@saude.gov>
2024-10-30 13:55:50 -0300

    Atualizar a pasta inteira

--- tests/test_unitarios.py
+++ tests/test_unitarios.py
@@ -1,8 +1,21 @@
-#from ETL.extract import ler_arquivo
-from .utils import gerar_dados
-
-dados_teste = gerar_dados()
-
-print(dados_teste.head())
+import pandas as pd
+from Pipeline.ETL.transform import converter_para_datas
 
 
+def test_converter_datas():
+
+    df = pd.DatFrame(
+        {
+            'dataSintomas': [
+                '2020-01-01',
+                '2021-03-05',
+                '2022-12-04',
+                '2023-04-15',
+            ],
+            'variavel_um': [1, 2, 3, 4],
+        }
+    )
+
+    converter_para_datas(df, 'dataSintomas', formato='%Y-%m-%d')
+
+    assert df['dataSintomas'].dtypes == 'datetime'

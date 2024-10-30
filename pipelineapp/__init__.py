0dc3f05c8fea6c556ed61431394f49b5aee071e3
Pedro <pedro.sa@saude.gov>
2024-10-30 13:55:23 -0300

    Atualizar a pasta inteira

--- pipelineapp/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Módulo para executar o pipeline completo."""
-
-from .etl import extract, load, pipeline, transform
-
-__all__ = ['extract', 'load', 'transform', 'pipeline']
--- pipelineapp/etl/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Módulos de ETL."""
-
-from .extract import *
-from .load import *
-from .pipeline import *
-from .transform import *
-
-__all__ = ['extract', 'load', 'pipeline', 'transform']

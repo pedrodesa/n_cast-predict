ec6bd50852a4dcfffda489b75a7cdc33c902ed93
Pedro de Sá <pedrodesa42@gmail.com>
2024-11-04 22:19:15 -0300

    Tabelas atualizadas

--- databases/create.sql
+++ databases/create.sql
@@ -8,25 +8,26 @@ CREATE DATABASE IF NOT EXISTS bd_cnie;
 
 -- Tabela de dados brutos de SRAG
 CREATE TABLE IF NOT EXISTS tb_covid (
-	dt_sin_pri DATE NULL,
-	dt_digita DATE NULL,
-	classi_fin INTEGER NULL,
-    evolucao INTEGER NULL,
-	co_mun_res VARCHAR(5) NULL,
-	sg_uf CHAR(2) NULL,
-	co_mun_not VARCHAR(7) NULL,
-    sg_uf_not CHAR(2) NULL,
-	idade_nasc INT NULL
+	dt_sin_pri DATE,
+	dt_digita DATE,
+	classi_fin INTEGER,
+    evolucao INTEGER,
+	co_mun_res VARCHAR(5),
+	sg_uf CHAR(2),
+	co_mun_not VARCHAR(7),
+    sg_uf_not CHAR(2),
+	idade_nasc INT
 );
 
 
 -- Tabela resultados predição
 CREATE TABLE IF NOT EXISTS tb_result_predicao (
-	Time INTEGER NULL,
-	dt_event DATE NULL,
-	Median NUMERIC NULL,
-	LI NUMERIC NULL,
-	LS NUMERIC NULL,
-	LIb NUMERIC NULL,
-	LSb NUMERIC NULL
+	Time INTEGER,
+	dt_event DATE,
+	Median NUMERIC,
+	LI NUMERIC,
+	LS NUMERIC,
+	LIb NUMERIC,
+	LSb NUMERIC
 );
+

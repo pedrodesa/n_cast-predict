9032ed8c6a2e6b056415c2ead78e5b3b13487ac0
Pedro Terra Teles de Sá <pedro.sa@saude.gov>
2025-01-07 10:34:43 -0300

    Tabelas ajustadas de acordo com o modelo relacional

--- databases/create.sql
+++ databases/create.sql
@@ -1,33 +1,47 @@
 -- BANCOS DE DADOS
 
--- DBTITLE; bd cnie
-CREATE DATABASE IF NOT EXISTS bd_cnie;
-
+-- DBTITLE
+CREATE DATABASE IF NOT EXISTS bd_nowcast_covid;
 
 -- TABELAS
 
--- Tabela de dados brutos de SRAG
-CREATE TABLE IF NOT EXISTS tb_covid (
-	dt_sin_pri DATE,
-	dt_digita DATE,
-	classi_fin INTEGER,
-    evolucao INTEGER,
-	co_mun_res VARCHAR(5),
-	sg_uf CHAR(2),
-	co_mun_not VARCHAR(7),
-    sg_uf_not CHAR(2),
-	idade_nasc INT
+-- Tabela de dados do e-SUS Notifica
+CREATE TABLE IF NOT EXISTS tb_esus_covid (
+	datainiciosintomas DATE,
+	datanotificacao DATE,
+	estadoibge INTEGER,
+    idade INTEGER
 );
 
+-- Tabela da série de semanas epidemiológicas
+CREATE TABLE IF NOT EXISTS tb_serie_semana_epi (
+	datainiciosintomas DATE,
+	estadoibge INTEGER,
+    faixaetaria VARCHAR,
+	datamodelagem DATE
+);
 
--- Tabela resultados predição
-CREATE TABLE IF NOT EXISTS tb_result_predicao (
+-- Tabela da série observada de semanas epidemiológicas
+CREATE TABLE IF NOT EXISTS tb_serie_semana_epi_obs (
+	datainiciosintomas DATE,
+	estadoibge INTEGER,
+    faixaetaria VARCHAR,
+	datamodelagem DATE,
+	CONSTRAINT pk_id PRIMARY KEY (estadoibge, faixaetaria, datamodelagem)
+);
+
+-- Tabela transformada para semanas epidemiológicas
+CREATE TABLE IF NOT EXISTS tb_nowcasting (
 	Time INTEGER,
 	dt_event DATE,
 	Median NUMERIC,
 	LI NUMERIC,
 	LS NUMERIC,
 	LIb NUMERIC,
-	LSb NUMERIC
+	LSb NUMERIC,
+	estadoibge INTEGER,
+	faixaetaria VARCHAR,
+	datamodelagem DATE,
+	CONSTRAINT fk_id FOREIGN KEY (estadoibge, faixaetaria, datamodelagem)
+	REFERENCES tb_serie_semana_epi_obs (estadoibge, faixaetaria, datamodelagem)
 );
-

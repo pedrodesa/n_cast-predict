df3ba0403ec0d6b0aadae4f688ab8659f30aecb1
Pedro Terra Teles de Sá <pedro.sa@saude.gov>
2025-01-08 09:59:59 -0300

    Script ajustado para mensagens, mas mensagens ainda estão dando erro

--- pipelineapp/etl/rdata2csv.r
+++ pipelineapp/etl/rdata2csv.r
@@ -6,12 +6,18 @@ setwd('D:/n_cast-predict')
 # Lista todos os arquivos com extensão Rdata no diretório './data'
 files <- list.files(path = './data', pattern = '\\.Rdata$', full.names = TRUE)
 
+# Verifica se há arquivos .Rdata no diretório
+if (length(files) == 0) {
+  stop("Nenhum arquivo .Rdata encontrado no diretório './data'.")
+}
+
 # Verifica se a pasta de saída existe; caso contrário, cria a pasta
 output_dir <- './data/output'
 if (!dir.exists(output_dir)) {
-  dir.create(output_dir, recursive = TRUE)
-  message("Diretório de saída criado: ", output_dir)
-}
+    # Cria o diretório, se ele não existir
+    dir.create(output_dir, recursive = TRUE)
+    message("Diretório de saída criado: ", output_dir)
+    }
 
 # Processa cada arquivo individualmente
 for (file in files) {
@@ -30,7 +36,7 @@ for (file in files) {
     # Verifica se o objeto é um data.frame
     if (is.data.frame(obj) && nrow(obj) > 0) {
       # Define o nome do arquivo CSV com base no nome do objeto
-      output_file <- file.path(output_dir, paste0(obj_name, "_dados_nowcasting.csv"))
+      output_file <- file.path(output_dir, paste0(obj_name, "_tipo_alterado.csv"))
       
       # Escreve o data.frame no arquivo CSV
       write.csv2(obj, output_file, row.names = FALSE)
